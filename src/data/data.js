74f9c5802db80f3bd9ee57a1f4d3901ce32e07fb
Mateo Ozino <mateo.ozino@gmail.com>
2024-02-29 14:44:17 -0300

    Cambios menores en la sección de proyectos.

--- src/data/data.js
+++ src/data/data.js
@@ -4,15 +4,13 @@ import mateoLogo from '../assets/portfolio-mateo/mateo-logo.png'
 import mateo1 from '../assets/portfolio-mateo/mateo1.png'
 import mateo2 from '../assets/portfolio-mateo/mateo2.png'
 import mateo3 from '../assets/portfolio-mateo/mateo3.png'
-import mateo4 from '../assets/portfolio-mateo/mateo4.png'
 import magicLogo from '../assets/magicpedia/magic-logo.png'
 import magic1 from '../assets/magicpedia/magic1.png'
 import magic2 from '../assets/magicpedia/magic2.png'
 import magic3 from '../assets/magicpedia/magic3.png'
-import magic4 from '../assets/magicpedia/magic4.png'
 
-const portfolioRedesingImages = [mateo1, mateo2, mateo3, mateo4]
-const magicpediaImages = [magic1, magic2, magic3, magic4]
+const portfolioRedesingImages = [mateo1, mateo2, mateo3]
+const magicpediaImages = [magic1, magic2, magic3]
 
 export const projects = [
   {
